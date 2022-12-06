9f1d47b51cf2e80db6336f108ffc375c93431b56
Christoph Fricke <christoph.fricke@holisticon.de>
2022-12-06 14:42:22 +0100

    feat: initialize Astro project

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "solid-js/jsx-runtime";
+
+export function Footer(): JSX.Element {
+  return (
+    <footer>
+      <span>&copy; Christoph Fricke</span>
+    </footer>
+  );
+}
